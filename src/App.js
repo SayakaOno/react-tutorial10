156720b58a0ba0e9c03cfdbb29c71601c0f567a4
Sayaka Ono <sayaka.ono.81@gmail.com>
2018-12-29 14:10:29 -0800

    add contact component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
 import React from "react";
+import Contact from "./Contact";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      password: "swordfish",
-      authorized: false
-    };
-    this.authorize = this.authorize.bind(this);
-  }
-
-  authorize(e) {
-    const password = e.target.querySelector('input[type="password"]').value;
-    const auth = password == this.state.password;
-    this.setState({
-      authorized: auth
-    });
-  }
-
-  render() {
-    return (
-      <div id="authorization">
-        <h1>Contact</h1>
-        <ul>
-          <li>client@example.com</li>
-          <li>555.555.5555</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <Contact />;
+};
 
 export default App;
